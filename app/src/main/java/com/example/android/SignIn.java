f007a21c98d0ea07ff36ee2905f667cf63e04568
Fahad Pathan <fahadpathan56@gmail.com>
2023-02-01 16:06:00 +0600

    added firebase
    1. authentication added.

--- app/src/main/java/com/example/android/SignIn.java
+++ app/src/main/java/com/example/android/SignIn.java
@@ -1,5 +1,6 @@
 package com.example.android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -10,6 +11,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignIn extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,11 +26,25 @@ public class SignIn extends AppCompatActivity implements View.OnClickListener {
     ImageView google, facebook;
     EditText email, password;
 
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            finish();
+            start_HomeScreenUser_activity();
+        }
+    }
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        mAuth = FirebaseAuth.getInstance();
 
         signUp = findViewById(R.id.signupID);
         logIn = findViewById(R.id.loginID);
@@ -38,11 +60,71 @@ public class SignIn extends AppCompatActivity implements View.OnClickListener {
         facebook.setOnClickListener(this);
     }
 
+    // to work with on Click listeners
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.signupID) {
-            Intent intent = new Intent(this, SignUp.class);
-            startActivity(intent);
+            finish();
+            start_SignUp_activity();
         }
+
+        if (v.getId() == R.id.loginID) {
+            getLogin("email");
+        }
+    }
+
+    // login for already registered users
+    private void getLogin(String type) {
+        String emailText = email.getText().toString();
+        String passText = password.getText().toString();
+
+        // checking for email input
+        if(emailText.isEmpty()) {
+            email.setError("Enter an email address");
+            email.requestFocus();
+            return;
+        }
+
+        // checking the email is valid or not
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
+            email.setError("Enter a valid email address");
+            email.requestFocus();
+            return;
+        }
+
+        // checking the validity of the password
+        if(passText.isEmpty() || passText.length() < 8) {
+            password.setError("minimum length is 8");
+            password.requestFocus();
+            return;
+        }
+
+        if (type.equals("email")) loginWithEmailAndPass(emailText, passText); // if tries to login with email and pass
+    }
+
+    // verification of valid users. (email and pass)
+    private void loginWithEmailAndPass(String emailText, String passText) {
+        mAuth.signInWithEmailAndPassword(emailText, passText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    finish();
+                    start_HomeScreenUser_activity();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Login failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void start_SignUp_activity() {
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+    }
+
+    public void start_HomeScreenUser_activity() {
+        Intent intent = new Intent(this, HomeScreen_user.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/android/SignUp.java
+++ app/src/main/java/com/example/android/SignUp.java
@@ -1,5 +1,6 @@
 package com.example.android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -10,6 +11,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,11 +28,15 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     ImageView google, facebook;
     EditText email, password;
 
+    private FirebaseAuth mAuth; // to work with firebase
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mAuth = FirebaseAuth.getInstance();
 
         signIn = findViewById(R.id.signInID);
         create = findViewById(R.id.createID);
@@ -31,6 +45,7 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         email = findViewById(R.id.emailID);
         password = findViewById(R.id.passwordID);
 
+        // setting on click listener
         create.setOnClickListener(this);
         signIn.setOnClickListener(this);
         google.setOnClickListener(this);
@@ -39,9 +54,71 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+        // if tries to go to the sign in page from sign up page
         if (v.getId() == R.id.signInID) {
-            Intent intent = new Intent(this, SignIn.class);
-            startActivity(intent);
+            finish();
+            start_SignIn_activity();
         }
+
+        // if anyone tries to create a new account with email and pass
+        if (v.getId() == R.id.createID) {
+            createAccount("email");
+        }
+    }
+
+    // to create a new account
+    private void createAccount(String type) {
+        String emailText = email.getText().toString().trim(); // text of email (input)
+        String passText = password.getText().toString(); // text of password (input)
+
+        // checking for email input
+        if(emailText.isEmpty()) {
+            email.setError("Enter an email address");
+            email.requestFocus();
+            return;
+        }
+
+        // checking the email is valid or not
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
+            email.setError("Enter a valid email address");
+            email.requestFocus();
+            return;
+        }
+
+        // checking the validity of the password
+        if(passText.isEmpty() || passText.length() < 8) {
+            password.setError("minimum length is 8");
+            password.requestFocus();
+            return;
+        }
+
+        if (type.equals("email")) createWithEmail(emailText, passText); // with email and pass
+    }
+
+    // if the user wants to create account with email and pass
+    private void createWithEmail(String emailText, String passText) {
+        // if every info is ok, creating a new account.
+        mAuth.createUserWithEmailAndPassword(emailText, passText)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign up success, update UI with the signed-in user's information
+                            Toast.makeText(getApplicationContext(), "Registration is successful", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign up fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "Already exists! Try sign in.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void createWithGoogle(String emailText, String passText) {
+
+    }
+
+    public void start_SignIn_activity() {
+        Intent intent = new Intent(this, SignIn.class);
+        startActivity(intent);
     }
 }
