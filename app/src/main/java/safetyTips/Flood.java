ef6dfbbf5b35db9cc62ed135336952f7a5accbe5
Sanzida Afrin Promi <sanzidaafrin5432@gmail.com>
2023-02-11 18:53:51 +0600

    Design of Flood's safety tips added

--- app/src/main/java/safetyTips/Flood.java
+++ app/src/main/java/safetyTips/Flood.java
@@ -1,16 +1,24 @@
 package safetyTips;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.android.R;
 import com.example.android.databinding.ActivityFloodBinding;
 
 import navigationBars.DrawerBaseActivity;
 
-public class Flood extends DrawerBaseActivity {
+public class Flood extends DrawerBaseActivity implements View.OnClickListener {
     ActivityFloodBinding activityFloodBinding;
+
+     Button beforeFloodButton;
+    Button duringFloodButton;
+    Button afterFloodButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +26,48 @@ public class Flood extends DrawerBaseActivity {
         activityFloodBinding=ActivityFloodBinding.inflate(getLayoutInflater());
         setContentView(activityFloodBinding.getRoot());
         allocateActivityTitle("Safety Tips for Flood");
+
+        beforeFloodButton=findViewById(R.id.beforeFloodButtonID);
+        duringFloodButton=findViewById(R.id.duringFloodButtonID);
+        afterFloodButton=findViewById(R.id.afterFloodButtonID);
+        beforeFloodButton.setOnClickListener(this);
+        duringFloodButton.setOnClickListener(this);
+        afterFloodButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.beforeFloodButtonID)
+        {
+            FragmentManager fragmentManager=getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainerView6,BeforeFlood.class,null)
+                    .setReorderingAllowed(true)
+                    .addToBackStack(" name")
+                    .commit();
+
+        }
+        if(v.getId()==R.id.duringFloodButtonID)
+        {
+            FragmentManager fragmentManager=getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainerView6,DuringFlood.class,null)
+                    .setReorderingAllowed(true)
+                    .addToBackStack(" name")
+                    .commit();
+
+        }
+        if(v.getId()==R.id.afterFloodButtonID)
+        {
+            FragmentManager fragmentManager=getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainerView6,AfterFlood.class,null)
+                    .setReorderingAllowed(true)
+                    .addToBackStack(" name")
+                    .commit();
+
+        }
+
     }
 }
